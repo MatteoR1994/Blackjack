3facd6960f98891cd350f92fbd165fda7848157e
Matteo Rinaldi <matteo.rinaldi1893@gmail.com>
2021-01-07 12:47:31 +0100

    Ricerca binaria e Trovare indice somma in array migliorato

--- src/main/kotlin/esercizi/main2.kt
+++ src/main/kotlin/esercizi/main2.kt
@@ -2,16 +2,16 @@ package esercizi
 
 fun main() {
     val numeri = intArrayOf(10,15,87,200,231)
-    val posizioneTarget: Int? = ricercaBinaria(numeri,15,0,numeri.size)
+    val posizioneTarget = ricercaBinaria(numeri,15,0,numeri.size)
     println("Array originale:")
     println("${numeri.joinToString(" - ")}\n")
-    println(posizioneTarget ?: "Non trovato.")
+    println(posizioneTarget)
 }
 
-fun ricercaBinaria(array: IntArray, target: Int, inizio: Int, fine: Int): Int? {
+fun ricercaBinaria(array: IntArray, target: Int, inizio: Int, fine: Int): Int {
     var centro: Int = (fine + inizio) / 2
     if(inizio >= fine) {
-        return null
+        return -1
     }
     if(array[centro] == target) {
         return centro
@@ -19,8 +19,7 @@ fun ricercaBinaria(array: IntArray, target: Int, inizio: Int, fine: Int): Int? {
     if(array[centro] > target) {
         return ricercaBinaria(array,target,inizio,centro)
     }
-    if(array[centro] < target) {
+    else {
         return ricercaBinaria(array,target,centro+1,fine)
     }
-    return null
 }
